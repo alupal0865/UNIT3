0827f2aa0930a55242c84d5c13c8505b58ce2f05
Tere_pallares <alupal0865@ieselcaminas.org>
2022-10-05 19:43:01 +0200

    EXAMPLE6.java

---
+++ src/EXAMPLE6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class EXAMPLE6 {
+    public static void main(String[] argv) {
+        float retailPrice;
+        float realPrice;
+        float discount;
+        System.out.println("Enter the retail price:");
+//Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        retailPrice = inputValue.nextFloat();
+        System.out.println("Enter the real price:");
+        realPrice = inputValue.nextFloat();
+        discount = (retailPrice - realPrice) / retailPrice * 100;
+        System.out.println("The discount is: “ + discount + “%");
+    }
+}
