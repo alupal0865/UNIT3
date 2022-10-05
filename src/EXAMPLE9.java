4ebff00fbe67b62702f236c47800430722ecf69b
Tere_pallares <alupal0865@ieselcaminas.org>
2022-10-05 22:09:47 +0200

    EXAMPLE9.java

---
+++ src/EXAMPLE9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class EXAMPLE9 {
+    public static void main(String[] argv) {
+        int age;
+        System.out.println("Enter your age:");
+//Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        age = inputValue.nextInt();
+        if
+        (age >= 18) {
+            Object you_have_the_legal_age;
+            System.out.println("you have the\n" +
+                    "legal age” if the age is >= 18 and “you are under legal age” otherwise.:");
+        } else {
+            System.out.println("you have the\n" +
+                    "legal age” if the age is >= 18 and “you are under legal age” otherwise.:");
+        }
+    }
+}
