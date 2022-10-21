c7c51ba845cfd8e90dfd8fc8fcba90f916e9a7de
Tere_pallares <alupal0865@ieselcaminas.org>
2022-10-21 20:19:57 +0200

    EXAMPLE14.java EXAMPLE14WITHERROR.java

---
+++ src/Example27.java
@@ -0,0 +1,17 @@
+public class Example27 {
+    public static void main(String [] argv) {
+        int i, a, b, c;
+        i = 1;
+        a = 0;
+        b = 1;
+        while (i <= 40) {
+            c = a;
+            a = b + a;
+            b = c;
+            System.out.println(c);
+            i = i + 1;
+
+        }
+    }
+}
+
